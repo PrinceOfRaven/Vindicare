ddb307bbd354112d0ca10d9d93b43c4b0639f387
alexsol.gotmogos <alexsol.gotmogos@gmail.com>
2026-04-29 00:51:26 +0700

    PauseMenu

--- Assets/Scripts/MainMenu/MainMenuControl.cs
+++ Assets/Scripts/MainMenu/MainMenuControl.cs
@@ -8,9 +8,11 @@ public class MainMenuControl : MonoBehaviour
         SceneManager.LoadScene("");
     }
 
+    //TODO: добавить систему сохранений и загружать их, а не сцену игры
     public void ContinueGame() 
     {
-        
+        SceneManager.LoadScene(0);
+        Debug.Log("Нажато");
     }
 
     public void ExitGromGame()
